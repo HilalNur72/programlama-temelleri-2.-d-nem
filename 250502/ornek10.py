e350cd1c0f0c545f0beba559028f7c6a1edcb433
HilalNur72 <hilalnursaci@gmail.com>
2025-05-02 11:59:45 +0300

    Add files via upload

---
+++ 250502/ornek10.py
@@ -0,0 +1,8 @@
+toplam=0
+s1=int(input("1.sayıyı girin :"))
+s2=int(input("2.sayıyı girin :"))
+for i in range(s1,s2):
+  ortalama=toplam/i
+adet= s1,s2 
+ortalama= toplam/adet
+print(ortalama)     
