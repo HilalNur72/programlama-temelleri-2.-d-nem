299abcd64283fd00514969ea32ba38fccbfe1e13
HilalNur72 <hilalnursaci@gmail.com>
2025-05-16 11:17:53 +0300

    Add files via upload

---
+++ 250411/ornek1.py
@@ -0,0 +1,7 @@
+sayi=float(input("sayıyı girin"))
+if sayi<0 :
+    print("sayı negatiftir")
+elif sayi>0 :
+    print("sayı pozitiftir")
+else:
+    print("sayı nötrdür")        
---
+++ 250411/ornek2.py
@@ -0,0 +1,9 @@
+plaka=int(input("plaka kodlarını giriniz"))
+if plaka=="06":
+    print("ankara")
+elif plaka=="07":
+    print("antalya")
+elif plaka=="08":
+    print("artvin")
+else:
+    print("Türkiye")    
